d851d611e4ec31b6b91cf497554f491717f6d8e4
Tóth András <tibor.andras.toth@idomsoft.hu>
2025-11-24 11:25:54 +0100

    Implement reactive stream processing with Flux in ReactorMain

--- src/main/java/employees/ReactorMain.java
+++ src/main/java/employees/ReactorMain.java
@@ -1,7 +1,16 @@
 package employees;
 
+import reactor.core.publisher.Flux;
+
 public class ReactorMain {
     public static void main(String[] args) {
+        //Lehetne stream is
+        //Stream.of(new Employee("John", 1980))
 
+        //Non blocking backpressure -  nem árasztjuk el a fogyasztót
+        Flux.just(new Employee("John", 1980))
+                .subscribe(
+                        System.out::println
+                );
     }
 }
