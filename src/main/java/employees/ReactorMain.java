018eaed7629f7c1a8d595653beb136df2a35389f
Tóth András <tibor.andras.toth@idomsoft.hu>
2025-11-24 11:40:00 +0100

    Remove unused Optional import in ReactorMain

--- src/main/java/employees/ReactorMain.java
+++ src/main/java/employees/ReactorMain.java
@@ -3,7 +3,6 @@ package employees;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.util.Optional;
 
 public class ReactorMain {
     public static void main(String[] args) {
